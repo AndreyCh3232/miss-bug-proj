17b8d1f04022dd9fb51e9b466e7a39f104914de5
Andrey Chetvergov <andrey32102@gmail.com>
2024-10-30 21:58:43 +0200

    Add logger.service.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import { bugService } from './services/bug.service.js'
+import { loggerService } from './services/logger.service.js'
 import PDFDocument from 'pdfkit'
 
 const app = express()
@@ -10,21 +11,32 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.get('/api/bug', (req, res) => {
-    bugService.query().then(bugs => res.send(bugs))
+    bugService.query()
+        .then(bugs => res.send(bugs))
+        .catch(err => {
+            loggerService.error('Failed to query bugs', err)
+            res.status(500).send('Cannot get bugs')
+        })
 })
 
 app.post('/api/bug/save', (req, res) => {
     const bug = req.body
     bugService.save(bug)
         .then(savedBug => res.send(savedBug))
-        .catch(err => res.status(500).send('Cannot save bug'))
+        .catch(err => {
+            loggerService.error('Failed to save bug', err)
+            res.status(500).send('Cannot save bug')
+        })
 })
 
 app.delete('/api/bug/:bugId/remove', (req, res) => {
     const { bugId } = req.params
     bugService.remove(bugId)
         .then(() => res.send('Bug removed'))
-        .catch(err => res.status(500).send('Cannot remove bug'))
+        .catch(err => {
+            loggerService.error(`Failed to remove bug with ID: ${bugId}`, err)
+            res.status(500).send('Cannot remove bug')
+        })
 })
 
 
@@ -35,6 +47,7 @@ app.get('/api/bug/:bugId', (req, res) => {
     if (!visitedBugs.includes(bugId)) {
         visitedBugs.push(bugId)
         if (visitedBugs.length > 3) {
+            loggerService.warn(`User visited more than 3 bugs: ${visitedBugs}`)
             return res.status(401).send('Wait for a bit')
         }
         res.cookie('visitedBugs', visitedBugs, { maxAge: 7000 })
@@ -42,7 +55,10 @@ app.get('/api/bug/:bugId', (req, res) => {
 
     bugService.getById(bugId)
         .then(bug => res.send(bug))
-        .catch(err => res.status(404).send('Bug not found'))
+        .catch(err => {
+            loggerService.error(`Bug not found with ID: ${bugId}`, err)
+            res.status(404).send('Bug not found')
+        })
 })
 
 app.get('/api/bug/download', (req, res) => {
@@ -64,6 +80,10 @@ app.get('/api/bug/download', (req, res) => {
         doc.pipe(res)
         doc.end()
     })
+        .catch(err => {
+            loggerService.error('Failed to generate PDF report', err)
+            res.status(500).send('Failed to generate PDF')
+        })
 })
 
 const port = 3030
