8abfaf458dfcdfa10f7b53666cd636f5c51c09fe
Andrey Chetvergov <andrey32102@gmail.com>
2024-10-30 15:23:10 +0200

    Add a cookie for usage limit

--- server.js
+++ server.js
@@ -30,3 +30,22 @@ app.get('/api/bug/:bugId/remove', (req, res) => {
     bugService.remove(req.params.bugId)
     res.send({ msg: 'Bug removed successfully' })
 })
+
+app.get('/api/bug/:bugId', (req, res) => {
+    const { bugId } = req.params
+    let visitedBugs = req.cookies.visitedBugs || []
+
+    if (!visitedBugs.includes(bugId)) {
+        visitedBugs.push(bugId)
+    }
+
+    if (visitedBugs.length > 3) {
+        return res.status(401).send('Wait for a bit')
+    }
+
+    res.cookie('visitedBugs', visitedBugs, { maxAge: 7000 })
+    console.log(`User visited bugs: ${visitedBugs}`)
+
+    const bug = bugService.getById(bugId)
+    res.json(bug)
+})
